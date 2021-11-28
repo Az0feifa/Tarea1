3a961b662041b691c34c26f0c6d47bcff2cad811
DjAzofeifa <geraldazofeifa@gmail.com>
2021-11-28 16:27:32 -0600

    Se reubica el pack de inicio y de index
    Se mueve el html, css y js de index ubicada en src para la carpeta inicio, y los html, css y js de inicio se mueve a src. Se corriguen los link de estilo y de js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function validate() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+
+	if (username == 'admin' && password == '123456') {
+		swal({
+			title: 'Bienvenido',
+			text: 'Sesión iniciada correctamente',
+			icon: 'success',
+		}).then(() => {
+			window.location.href = './pages/inicio/inicio.html';
+		});
+	} else {
+		swal({
+			title: 'Error',
+			text: 'Usuario o contraseña incorrectos',
+			icon: 'error',
+		}).then(() => {
+			return false;
+		});
+	}
+}
