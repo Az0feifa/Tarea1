a54a03d181b600cc29780fd17fdbd5c3b85580b0
DjAzofeifa <geraldazofeifa@gmail.com>
2021-11-28 15:20:34 -0600

    Se elimina el form y el submit
    Como no se esta usando jquery, al clickear submit en JS recarga la pagina, evitando se ejecute la funcion.

--- src/pages/Inicio/inicio.js
+++ src/pages/Inicio/inicio.js
@@ -1,20 +1,22 @@
-function IniciarSesion() {
-	usuario = document.getElementById('username').value;
-	contrasena = document.getElementById('password').value;
+function validate() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
 
-	if (usuario == 'admin' && contrasena == '123456') {
+	if (username == 'admin' && password == '123456') {
 		swal({
 			title: 'Bienvenido',
 			text: 'Sesión iniciada correctamente',
 			icon: 'success',
-		}).then(()=>{
-		window.location.href = '../../index.html';
-        })
+		}).then(() => {
+			window.location.href = 'https://www.google.com/';
+		});
 	} else {
 		swal({
 			title: 'Error',
 			text: 'Usuario o contraseña incorrectos',
 			icon: 'error',
+		}).then(() => {
+			return false;
 		});
 	}
 }
